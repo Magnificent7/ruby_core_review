547586494879a0d4bda312f27c2ee9f9748cd859
dani <dani.zaghian@gmail.com>
2017-04-24 21:00:17 -0700

    added attr_accessor

--- employees.rb
+++ employees.rb
@@ -21,26 +21,12 @@
 
 #represent an employee as a class instance
 class Employee
+  attr_accessor :first_name, :last_name, :salary, :active
+
   def initialize(input_first_name, input_last_name, input_salary, input_active)
     @first_name = input_first_name
     @last_name = input_last_name
     @salary = input_salary
-    @active = input_active
-  end
-
-  def first_name
-    @first_name
-  end
-
-  def last_name
-    @last_name
-  end
-
-  def salary
-    @salary
-  end
-
-  def active=(input_active)
     @active = input_active
   end
 
